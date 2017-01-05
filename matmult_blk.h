ce6b419b67f3ef67000369e90ab75f4a568c3292
Wacim Belblidia <wacim.bel@gmail.com>
2017-01-05 17:01:03 +0100

    update of the header for blocking function

--- matmult_blk.h
+++ matmult_blk.h
@@ -1,6 +1,6 @@
 #ifndef __MATMULT_BLK_H
 #define __MATMULT_BLK_H
 
-void matmult_blk(int m, int nb1, int n, int nb2, int k, int nb3, double **A, double **B, double **C);
+void matmult_blk(int m, int n, int k, int nb, double **A, double **B, double **C);
 
 #endif
