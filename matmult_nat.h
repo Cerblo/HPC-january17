c610febd77ed13263d6c79378770cd8ee3ab412c
Hippolyte Heuberger <hippolyte.heuberger@gmail.com>
2017-01-05 10:56:38 +0100

    change of header to take account of permutations

--- matmult_nat.h
+++ matmult_nat.h
@@ -2,5 +2,11 @@
 #define __MATMULT_NAT_H
 
 void matmult_nat(int m, int n, int k, double **A, double **B, double **C);
+void matmult_mnk(int m, int n, int k, double **A, double **B, double **C);
+void matmult_mkn(int m, int n, int k, double **A, double **B, double **C);
+void matmult_kmn(int m, int n, int k, double **A, double **B, double **C);
+void matmult_knm(int m, int n, int k, double **A, double **B, double **C);
+void matmult_nmk(int m, int n, int k, double **A, double **B, double **C);
+void matmult_nkm(int m, int n, int k, double **A, double **B, double **C);
 
 #endif
