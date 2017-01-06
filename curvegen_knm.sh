d71affeec7489f153f647a09b5b53efdc6dab9ff
Hippolyte Heuberger <hippolyte.heuberger@gmail.com>
2017-01-06 13:25:24 +0100

    change of makefile.gcc

---
+++ curvegen_knm.sh
@@ -0,0 +1,18 @@
+matmult_c.gcc knm 20 20 22
+matmult_c.gcc knm 30 20 35
+matmult_c.gcc knm 40 30 25
+matmult_c.gcc knm 40 30 33
+matmult_c.gcc knm 50 30 32
+matmult_c.gcc knm 70 60 64
+matmult_c.gcc knm 90 80 78
+matmult_c.gcc knm 100 80 90
+matmult_c.gcc knm 100 90 101
+matmult_c.gcc knm 120 100 91
+matmult_c.gcc knm 300 300 267
+matmult_c.gcc knm 500 400 455
+matmult_c.gcc knm 800 700 751
+matmult_c.gcc knm 1000 800 892
+matmult_c.gcc knm 1200 1000 1200
+matmult_c.gcc knm 4000 2000 4917
+matmult_c.gcc knm 6000 3000 5917
+
