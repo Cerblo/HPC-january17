3a44761d981a6e2a6d1dbe4e04a650aea62330f1
Maximilian Zellinger <mzell@live.at>
2017-01-06 11:22:50 +0100

    runex update

--- runex.sh
+++ runex.sh
@@ -6,52 +6,35 @@
 
 # Running experiment 15x and save results
 
-#1
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#2
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#3
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#4
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#5
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-
-#1
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#2
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#3
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#4
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#5
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-
-#1
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#2
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#3
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#4
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
-#5
-./matmult_c.gcc per mnk >> results_perm.dat
-cat mnk >> results_perm.dat
+cat 1 >> per_results.dat
+matmult_c.gcc per 4 5 9 >> per_results.dat
+cat 2 >> per_results.dat
+matmult_c.gcc per 8 8 9 >> per_results.dat
+cat 3 >> per_results.dat
+matmult_c.gcc per 13 13 13 >> per_results.dat
+cat 4 >> per_results.dat
+matmult_c.gcc per 17 17 21 >> per_results.dat
+cat 5 >> per_results.dat
+matmult_c.gcc per 23 26 29 >> per_results.dat
+cat 6 >> per_results.dat
+matmult_c.gcc per 50 35 39 >> per_results.dat
+cat 7 >> per_results.dat
+matmult_c.gcc per 60 40 46 >> per_results.dat
+cat 8 >> per_results.dat
+matmult_c.gcc per 80 40 57 >> per_results.dat
+cat 9 >> per_results.dat
+matmult_c.gcc per 100 70 77 >> per_results.dat
+cat 10 >> per_results.dat
+matmult_c.gcc per 120 100 82 >> per_results.dat
+cat 11 >> per_results.dat
+matmult_c.gcc per 150 100 140 >> per_results.dat
+cat 12 >> per_results.dat
+matmult_c.gcc per 150 200 200 >> per_results.dat
+cat 13 >> per_results.dat
+matmult_c.gcc per 300 200 280 >> per_results.dat
+cat 14 >> per_results.dat
+matmult_c.gcc per 500 300 438 >> per_results.dat
+cat 15 >> per_results.dat
+matmult_c.gcc per 600 500 637 >> per_results.dat
 
 exit 0
