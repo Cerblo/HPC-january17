443a3c0b56781aa7cd02cbbe2a5f724bee1950da
Hippolyte Heuberger <hippolyte.heuberger@gmail.com>
2017-01-06 12:59:31 +0100

    addition of some scripts for curve generating

---
+++ curvegen_lib.sh
@@ -0,0 +1,18 @@
+matmult_c.gcc lib 20 20 22 
+matmult_c.gcc lib 30 20 35 
+matmult_c.gcc lib 40 30 25 
+matmult_c.gcc lib 40 30 33 
+matmult_c.gcc lib 50 30 32 
+matmult_c.gcc lib 70 60 64 
+matmult_c.gcc lib 90 80 78 
+matmult_c.gcc lib 100 80 90 
+matmult_c.gcc lib 100 90 101 
+matmult_c.gcc lib 120 100 91 
+matmult_c.gcc lib 300 300 267 
+matmult_c.gcc lib 500 400 455 
+matmult_c.gcc lib 800 700 751 
+matmult_c.gcc lib 1000 800 892 
+matmult_c.gcc lib 1200 1000 1200 
+matmult_c.gcc lib 4000 2000 4917 
+matmult_c.gcc lib 6000 3000 5917 
+
---
+++ curvegen_nkm.sh
@@ -0,0 +1,18 @@
+matmult_c.gcc blk 20 20 22 36
+matmult_c.gcc blk 30 20 35 36
+matmult_c.gcc blk 40 30 25 36
+matmult_c.gcc blk 40 30 33 36
+matmult_c.gcc blk 50 30 32 36
+matmult_c.gcc blk 70 60 64 36
+matmult_c.gcc blk 90 80 78 36
+matmult_c.gcc blk 100 80 90 36
+matmult_c.gcc blk 100 90 101 36
+matmult_c.gcc blk 120 100 91 36
+matmult_c.gcc blk 300 300 267 36
+matmult_c.gcc blk 500 400 455 36
+matmult_c.gcc blk 800 700 751 36
+matmult_c.gcc blk 1000 800 892 36
+matmult_c.gcc blk 1200 1000 1200 36
+matmult_c.gcc blk 4000 2000 4917 36
+matmult_c.gcc blk 6000 3000 5917 36
+
