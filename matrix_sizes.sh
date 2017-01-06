0d5bb634933ae830d7a0855c5198a732057deff0
Hippolyte Heuberger <hippolyte.heuberger@gmail.com>
2017-01-06 11:09:09 +0100

    change of extension size

---
+++ matrix_sizes.sh
@@ -0,0 +1,15 @@
+matmul_gcc.c blk 8 8 9
+matmul_gcc.c blk 13 13 13
+matmul_gcc.c blk 17 17 21
+matmul_gcc.c blk 23 26 29
+matmul_gcc.c blk 50 35 39
+matmul_gcc.c blk 60 40 46
+matmul_gcc.c blk 80 40 57
+matmul_gcc.c blk 100 70 77
+matmul_gcc.c blk 120 100 82   
+matmul_gcc.c blk 150 100 140
+matmul_gcc.c blk 150 200 200
+matmul_gcc.c blk 300 200 280
+matmul_gcc.c blk 500 300 438
+matmul_gcc.c blk 600 500 637
+
