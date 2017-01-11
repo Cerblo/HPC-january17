e5d1c62053fd3603ca75ff0b00e586c438e8ae74
Maximilian Zellinger <mzell@live.at>
2017-01-11 15:29:11 +0100

    f_ij implemented, updated jacobian.h header file git push

--- assign2/jacobian.c
+++ assign2/jacobian.c
@@ -1,34 +1,49 @@
 #include jacobian.h
 
-void jacobian(double **OLD, double **NEW, int size, double TOL) {
+void jacobian(double **OLD, double **NEW, int size, double TOL, int max_it) {
 
   /* initializing iteration variables */
   int k = 0;
-  int max_it = 1000000;
 
-  while d > TOL && k < max_it {
+  /* stopping criterion is squared; this avoids square root calculation */
+  TOL2 = pow(TOL,2);
+
+  while d > TOL2 && k < max_it {
     OLD = NEW;
     /* update step */
-    OLD = jac_update(OLD, NEW, TOL);
-    d =
+    d = jac_update(OLD, NEW, size, h);
     k++;
   }
 }
 
-void jac_update(double **OLD, double **NEW, int size, double h) {
+
+/* jac_update updates the matrix NEW with values calculated from OLD
+   jac_update returns the summed absolute squared error */
+double jac_update(double **OLD, double **NEW, int size, double h) {
+
   /* loop counters over matrix entries */
   int i, j;
+  int err = 0;
 
   for (i = 0; i < size; i++) {
     for (j = 0; j < size; j++) {
-      NEW = 0.25*(OLD[i-1][j]+OLD[i+1][j]+OLD[i][j-1]+OLD[i][j+1]+\
-                  pow(h,2)*f_ij(i, j, h))
+      NEW[i][j] = 0.25*(OLD[i-1][j]+OLD[i+1][j]+OLD[i][j-1]\
+                  +OLD[i][j+1]+pow(h,2)*f_ij(i, j, size))
+      err += pow(abs(OLD[i][j] - NEW[i][j]), 2);
     }
   }
+  return err;
 }
 
-double f_ij(int i, int j, double h) {
+double f_ij(int i, int j, int N) {
+  /* relation between indexes and position in the square */
+  double x = ((2*i)/(N+1)) - 1
+  double y = ((2*j)/(N+1)) - 1
 
-
-  return f
+  if (x>=0 && x<=-1/3 && y<=-1/3 && y>=-2/3) {
+  	return 200;
+  }
+  else {
+  	return 0;
+  }
 }
--- assign2/jacobian.h
+++ assign2/jacobian.h
@@ -2,6 +2,6 @@
 #include <math.h>
 #include <stdlib.h>
 
-void jacobian(double **OLD, double **NEW, size, TOL);
-void jac_update();
-double f_ij();
+void jacobian(double **OLD, double **NEW, int size, double TOL, int max_it);
+void jac_update(double **OLD, double **NEW, int size, double h);
+double f_ij(int i, int j, int size, double h);
