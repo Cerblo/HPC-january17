cb4f7f3015d74b7b8b1cbe9d24bf429c12d05c63
Maximilian Zellinger <mzell@live.at>
2017-01-13 16:51:33 +0100

    v2 cosmetic changes

--- assign2/parallelization_v2/jacobian.c
+++ assign2/parallelization_v2/jacobian.c
@@ -4,9 +4,9 @@
 
 
 void mat_swap(double ***A, double ***B) {
-double **temp = *A;
-*A = *B;
-*B  = temp;
+  double **temp = *A;
+  *A = *B;
+  *B  = temp;
 }
 
 void jacobian(double **OLD, double **NEW, double **f, int size, double TOL, int max_it, \
@@ -14,44 +14,32 @@ void jacobian(double **OLD, double **NEW, double **f, int size, double TOL, int 
 
   /* initializing iteration variables */
   int k = 0;
-int i,j;
-double err;
-
-  /* stopping criterion is squared; this avoids square root calculation */
-  
+  int i,j;
+  double err;
   double d = TOL + 1;
 
-	#pragma omp parallel default(none) \
- shared(OLD, NEW, f, size, h,d, k, TOL, max_it, err) \
-private(i, j)
-{
+  #pragma omp parallel default(none) \
+              shared(OLD, NEW, f, size, h,d, k, TOL, max_it, err) \
+              private(i, j)
+  {
   while (d > TOL && k < max_it) {
-    /* update step */
-	err = 0;
-#pragma omp for reduction(+:err)
-  for (i = 1; i < size - 1; i++) {
-    for (j = 1; j < size - 1; j++) {
-
-	
-      NEW[i][j] = 0.25*(OLD[i-1][j]+OLD[i+1][j]+OLD[i][j-1]\
-                  +OLD[i][j+1]+h * h * f[i][j]);
-
-      err += pow(OLD[i][j] - NEW[i][j], 2);
-	
+    err = 0;
+    #pragma omp for reduction(+:err)
+    for (i = 1; i < size - 1; i++) {
+      for (j = 1; j < size - 1; j++) {
+        NEW[i][j] = 0.25*(OLD[i-1][j]+OLD[i+1][j]+OLD[i][j-1]\
+                          +OLD[i][j+1]+h * h * f[i][j]);
+        err += pow(OLD[i][j] - NEW[i][j], 2);
+      }
     }
+    d = 1./(size-2) * sqrt(err);
+    #pragma omp master
+    {
+    mat_swap(&OLD, &NEW);
+	  k++;
+  } /* end pragma omp master */
+    #pragma omp barrier
   }
-	d = 1./(size-2) * sqrt(err);
-#pragma omp master 
-{
-    	mat_swap(&OLD, &NEW);
-	k++;
-
-}
-#pragma omp barrier
-
-}
-}
+} /* end pragma omp parallel */
 printf("%i  %f\n", k, d);
-
-
 }
