d74c55023a3131f0112718885b6bf04a9aa65259
Maximilian Zellinger <mzell@live.at>
2017-01-13 16:54:13 +0100

    v3 cosmetic changes

--- assign2/parallelization_v2/main.c
+++ assign2/parallelization_v2/main.c
@@ -6,40 +6,35 @@
 #include "jacobian.h"
 #include <omp.h>
 
-int
-main(int argc, char **argv) {
+int main(int argc, char **argv) {
 
+  int size, N, max_it;
+  double h, guess,tol;
+  double **u_old, **u_new, **f;
 
+  if (argc < 4) {
+  	printf("Wrong input\n");
+  	return 0;
+  }
 
- 
-int size, N, max_it;
-double h, guess,tol;
-double **u_old, **u_new, **f;
+  N = atoi(argv[1]);
+  guess = 15;
 
-if (argc < 4) {
-	printf("Wrong input\n");
-	return 0;
-}
+  tol = atof(argv[3]);
+  max_it = atoi(argv[2]);
 
-N = atoi(argv[1]);
-guess = 15;
+  size = N + 2;
+  h = 2.0 / (N + 1);
 
-tol = atof(argv[3]);
-max_it = atoi(argv[2]);
+  u_old = malloc_2d(size, size);
+  u_new = malloc_2d(size, size);
+  f = malloc_2d(size, size);
 
-size = N + 2;
-h = 2.0 / (N + 1);
+  init_u(size, u_old, guess);
+  init_u(size, u_new, guess);
+  init_f(N, f);
 
+  jacobian(u_old, u_new, f, size, tol, max_it, h);
 
-u_old = malloc_2d(size, size); 
-u_new = malloc_2d(size, size); 
-f = malloc_2d(size, size); 
-
-init_u(size, u_old, guess);
-init_u(size, u_new, guess);
-init_f(N, f);
-
-jacobian(u_old, u_new, f, size, tol, max_it, h);
-
-return 0;
+  return 0;
 }
