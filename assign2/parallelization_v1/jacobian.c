3c72cde4f29cfc9f605183912347d06b0b03c3f3
Hippolyte Heuberger <hippolyte.heuberger@gmail.com>
2017-01-13 10:02:31 +0100

    put the er report to analyze

---
+++ assign2/parallelization_v1/jacobian.c
@@ -0,0 +1,61 @@
+#include <math.h>
+#include <stdio.h>
+#include <omp.h>
+
+
+/* jac_update updates the matrix NEW with values calculated from OLD
+   jac_update returns the summed absolute squared error */
+double jac_update(double **OLD, double **NEW, double **f, int size, double h) {
+
+  /* loop counters over matrix entries */
+  int i, j;
+  double err = 0;
+  double delta = pow(h,2);
+
+#pragma omp parallel for default(none) shared(NEW, OLD, f, delta, size) \
+	private(i, j) reduction(+:err)
+  for (i = 1; i < size - 1; i++) {
+    for (j = 1; j < size - 1; j++) {
+
+	
+      NEW[i][j] = 0.25*(OLD[i-1][j]+OLD[i+1][j]+OLD[i][j-1]\
+                  +OLD[i][j+1]+delta*f[i][j]);
+
+      err += pow(OLD[i][j] - NEW[i][j], 2);
+	
+    }
+  }
+
+	err = 1/pow(size-2,2) * err;
+	err = sqrt(err);
+  return err;
+}
+
+void mat_swap(double ***A, double ***B) {
+double **temp = *A;
+*A = *B;
+*B  = temp;
+}
+
+void jacobian(double **OLD, double **NEW, double **f, int size, double TOL, int max_it, \
+              double h) {
+
+  /* initializing iteration variables */
+  int k = 0;
+
+  /* stopping criterion is squared; this avoids square root calculation */
+  
+  double d = TOL + 1;
+
+  while (d > TOL && k < max_it) {
+    /* update step */
+    	d = jac_update(OLD, NEW, f, size, h);
+    	mat_swap(&OLD, &NEW);
+
+
+	k++;
+  }
+printf("%i  %f\n", k, d);
+
+
+}
---
+++ assign2/parallelization_v1/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "datatools.h"
+#include "init.h"
+#include "jacobian.h"
+/*
+#include <time.h>
+#define mytimer clock
+#define delta_t(a,b) (1e3 * (b - a) / CLOCKS_PER_SEC)
+*/
+int
+main(int argc, char **argv) {
+
+
+
+ 
+int size, N, max_it;
+double h, guess,tol;
+double **u_old, **u_new, **f;
+
+if (argc < 4) {
+	printf("Wrong input\n");
+	return 0;
+}
+
+N = atoi(argv[1]);
+guess = 15;
+
+tol = atof(argv[3]);
+max_it = atoi(argv[2]);
+
+//clock_t t1 = mytimer();
+size = N + 2;
+h = 2.0 / (N + 1);
+u_old = malloc_2d(size, size); 
+u_new = malloc_2d(size, size); 
+f = malloc_2d(size, size); 
+
+init_u(size, u_old, guess);
+init_u(size, u_new, guess);
+init_f(N, f);
+/*
+clock_t t2 = mytimer();
+printf("time for initialisation: %f\n", delta_t(t1,t2));
+*/
+jacobian(u_old, u_new, f, size, tol, max_it, h);
+
+return 0;
+}
