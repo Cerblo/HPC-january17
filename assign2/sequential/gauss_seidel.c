fa738cef21daf3a4c10c54aa7e28e36655f21392
Maximilian Zellinger <mzell@live.at>
2017-01-13 09:53:54 +0100

    cosmetic code changes

--- assign2/sequential/gauss_seidel.c
+++ assign2/sequential/gauss_seidel.c
@@ -1,65 +1,45 @@
 #include <math.h>
 #include <stdio.h>
-<<<<<<< HEAD
 #include <time.h>
+#include "datatools.h"
 
 #define mytimer clock
 #define delta_t(a,b) (1e3 * (b - a) / CLOCKS_PER_SEC)
 
-/* f_ij returns the value of the function for the respective coordinates */
-double f_ij_gauss(int i, int j, int N) {
-  /* relation between indexes and position in the square */
-  double x = ((2*(double) i)/((double)N+1)) - 1;
-  double y = ((2*(double) j)/((double)N+1)) - 1;
 
-  if (x>=0. && x<=1./3. && y<=-1./3. && y>=-2./3.) {
-  	return (double)200;
-  }
-  else {
-  	return (double)0;
-  }
-}
-
-=======
-#include "datatools.h"
->>>>>>> 4845ef154d382050836f441733db89e61ca2167b
-
-/* jac_update updates the matrix NEW with values calculated from OLD
-   jac_update returns the summed absolute squared error */
+/* gauss_update updates the grid matrix;
+   gauss_update returns the summed absolute squared error */
 double gauss_update(double **NEW, double **f, int size, double h) {
 
-  /* loop counters over matrix entries */
+  /* variable and timer declaration */
   int i, j;
   double err = 0;
   double delta = pow(h,2);
-	double temp ;  
+	double temp ;
 	double iter_time;
 	clock_t t1,t2;
 	t1 = mytimer();
 
-//disp_2d(size, size, NEW);
+  /* looping through inner grid and updating grid points */
   for (i = 1; i < size - 1; i++) {
     for (j = 1; j < size - 1; j++) {
-
-	temp = NEW[i][j];
+      temp = NEW[i][j];
       NEW[i][j] = 0.25*(NEW[i-1][j]+NEW[i+1][j]+NEW[i][j-1]\
                   +NEW[i][j+1]+delta*f[i][j]);
-
       err += pow(temp - NEW[i][j], 2);
     }
   }
 
-
 	t2 = mytimer();
 	iter_time = delta_t(t1,t2);
 	printf("Gau %d %f %f \n", size -2 , iter_time, 4*pow(size,2)/iter_time);
-	err = 1/pow(size-2,2) * err;
+
+  /* error calculation */
+  err = 1/pow(size-2,2) * err;
 	err = sqrt(err) ;
 
   return err;
 }
-
-
 
 
 void gauss_seidel(double **OLD, double **NEW, double **f, int size, double TOL, int max_it, \
@@ -67,9 +47,6 @@ void gauss_seidel(double **OLD, double **NEW, double **f, int size, double TOL, 
 
   /* initializing iteration variables */
   int k = 1;
-
-  /* stopping criterion is squared; this avoids square root calculation */
- 
   double d = TOL + 1;
 
   while (d > TOL && k < 5) {
--- assign2/sequential/main.c
+++ assign2/sequential/main.c
@@ -9,47 +9,46 @@
 int
 main(int argc, char **argv) {
 
-
-
- 
 int size, N, max_it;
 double h, guess,tol;
 double **u_old, **u_new, **f;
 
+/* user input check */
 if (argc < 5 || (strcmp(argv[1],"jac") != 0 && strcmp(argv[1],"gau") != 0)) {
 	printf("Wrong input\n");
 	return 0;
 }
 
+/* var declaration */
 N = atoi(argv[2]);
 guess = 15;
-
 tol = atof(argv[4]);
 max_it = atoi(argv[3]);
-
-
 size = N + 2;
 h = 2.0 / (N + 1);
-u_old = malloc_2d(size, size); 
-u_new = malloc_2d(size, size); 
-f = malloc_2d(size, size); 
 
+/* allocating space for matrices */
+u_old = malloc_2d(size, size);
+u_new = malloc_2d(size, size);
+f = malloc_2d(size, size);
+
+/* initializing matrices and function values */
 init_u(size, u_old, guess);
 init_u(size, u_new, guess);
 init_f(N, f);
 
-
-//disp_2d(size, size, f);
-
+/* algorithm selection by user */
 if (strcmp(argv[1],"jac") == 0){
-
 	jacobian(u_old, u_new, f, size, tol, max_it, h);
-
 }
-else{
+else {
+	gauss_seidel(u_old, u_new, f, size, tol, max_it, h);
+}
 
-	gauss_seidel(u_old, u_new, f, size, tol, max_it, h);}
-//disp_2d(size, size, u_new);
+/* freeing allocated space */
+free_2d(u_new);
+free_2d(u_old);
+free_2d(f);
 
 return 0;
 }
