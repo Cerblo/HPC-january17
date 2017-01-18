95164bbe6263e3027878e4b5a51f08be9e687ddb
Wacim Belblidia <wacim.bel@gmail.com>
2017-01-18 09:56:42 +0100

    Week3 start

---
+++ assign3/test.cu
@@ -0,0 +1,20 @@
+int main(int argc, char **argv)
+{
+ // Allocate memory space on host and device
+ h_data = malloc(...);
+ cudaMalloc(...);
+
+ // Transfer data from host to device
+ cudaMemcpy(...);
+
+ // Kernel lauch
+ kernel<<<Grid, Block>>>(...);
+ cudaDeviceSynchronize();
+
+ // Transfer results from device to host
+ cudaMemcpy(...);
+
+ // Free memory
+ free(h_data);
+ cudaFree(...);
+} 
