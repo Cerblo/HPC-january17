bb1481750e5317d6813a5da886518ced04088845
Hippolyte Heuberger <hippolyte.heuberger@gmail.com>
2017-01-18 15:38:54 +0100

    new cu files

---
+++ assign3/poisson/gpu1/main.cu
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include "datatools.h"
+#include "init.h"
+#include "jacobian.h"
+#include <helper_cuda.h>
+
+int main(int argc, char **argv) {
+
+  int size, N, max_it, total_size, k;
+  double h, guess;
+  double *h_Uold, *h_Unew, *h_f;
+  double *d_Uold, *d_Unew, *d_f;
+
+  if (argc < 3) {
+  	printf("Wrong input\n");
+  	return 0;
+  }
+
+  N = atoi(argv[1]);
+  guess = 15;
+
+  max_it = atoi(argv[2]);
+
+  size = N + 2;
+  h = 2.0 / (N + 1);
+total_size = size * size * sizeof(double);
+
+  cudaMallocHost((void**)&h_Uold, total_size);
+  cudaMallocHost((void**)&h_Unew, size * size);
+  cudaMallocHost((void**)&h_f, total_size);
+
+cudaMalloc((void**)&d_Uold, total_size);
+cudaMalloc((void**)&d_Unew, total_size);
+cudaMalloc((void**)&d_f, total_size);
+
+  init_u(size, h_Uold, guess);
+  init_u(size, h_Unew, guess);
+  init_f(N, h_f);
+
+cudaMemcpy(d_Uold, h_Uold, total_size, cudaMemcpyHostToDevice); 
+cudaMemcpy(d_f, h_f, total_size, cudaMemcpyHostToDevice); 
+
+
+k = 0;
+while(k < max_it) {
+
+mat_swap(&h_Uold, &h_Unew);
+
+cudaMemcpy(d_Unew, h_Unew, total_size, cudaMemcpyHostToDevice); 
+
+  jacobian<<<1, 1>>>(d_Uold, d_Unew, d_f, size, max_it, h);
+cudaCheckErrors( cudaDeviceSynchronize() );
+
+cudaMemcpy(h_Unew, d_Unew, total_size, cudaMemcpyDeviceToHost); 
+
+k++;
+}
+
+
+int i;
+for (i = 0; i < size * size; i++) {
+if (i % size == 0)
+printf("\n");
+printf("%5.1f ", h_Unew[i]);}
+printf("\n");
+
+
+cudaFreeHost(h_Uold);
+cudaFreeHost(h_Unew);
+cudaFreeHost(h_f);
+
+cudaFree(d_Uold);
+cudaFree(d_Unew);
+cudaFree(d_f);
+
+  return 0;
+}
