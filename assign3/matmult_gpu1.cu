031c85e0172f1d4f6a857648c1b1d26f2fe7f4d6
Maximilian Zellinger <mzell@live.at>
2017-01-18 15:07:06 +0100

    first attempt

--- assign3/matmult_gpu1.cu
+++ assign3/matmult_gpu1.cu
@@ -15,7 +15,7 @@ __global__ void kernel_gpu1(int m, int n, int k, double* A, double* B, double* C
 	for ( i = 0; i < m; i++ ) {
 		for ( j = 0; j < n; j++ ) {
 			for ( l = 0; l < k; l++) {
-				C[i][j] += A[i][l]*B[l][j];
+				C[i*j+i] += A[i*l+i]*B[l*j+l];
 			}
 		}
 	}
