a01c6dcba3dee07bc8dc53184abdab0ca5c53f2a
Hippolyte Heuberger <hippolyte.heuberger@gmail.com>
2017-01-20 19:59:04 +0100

    code ready to compress

--- assign3/poisson/gpu1/main.cu
+++ assign3/poisson/gpu1/main.cu
@@ -25,21 +25,21 @@ int main(int argc, char **argv) {
   h = 2.0 / (N + 1);
 total_size = size * size * sizeof(double);
 
-  checkCudaErrors(cudaMallocHost((void**)&h_Uold, total_size));
-  checkCudaErrors(cudaMallocHost((void**)&h_Unew, total_size));
-  checkCudaErrors(cudaMallocHost((void**)&h_f, total_size));
+  cudaMallocHost((void**)&h_Uold, total_size);
+  cudaMallocHost((void**)&h_Unew, total_size);
+  cudaMallocHost((void**)&h_f, total_size);
 
-checkCudaErrors(cudaMalloc((void**)&d_Uold, total_size));
-checkCudaErrors(cudaMalloc((void**)&d_Unew, total_size));
-checkCudaErrors(cudaMalloc((void**)&d_f, total_size));
+cudaMalloc((void**)&d_Uold, total_size);
+cudaMalloc((void**)&d_Unew, total_size);
+cudaMalloc((void**)&d_f, total_size);
 
   init_u(size, h_Uold, guess);
   init_u(size, h_Unew, guess);
   init_f(N, h_f);
 
-checkCudaErrors(cudaMemcpy(d_Uold, h_Uold, total_size, cudaMemcpyHostToDevice));
-checkCudaErrors(cudaMemcpy(d_Unew, h_Unew, total_size, cudaMemcpyHostToDevice)); 
-checkCudaErrors(cudaMemcpy(d_f, h_f, total_size, cudaMemcpyHostToDevice)); 
+cudaMemcpy(d_Uold, h_Uold, total_size, cudaMemcpyHostToDevice);
+cudaMemcpy(d_Unew, h_Unew, total_size, cudaMemcpyHostToDevice); 
+cudaMemcpy(d_f, h_f, total_size, cudaMemcpyHostToDevice); 
 
 
 k = 0;
@@ -48,28 +48,21 @@ while(k < max_it) {
 mat_swap(&d_Uold, &d_Unew);
 
   jacobian<<<1, 1>>>(d_Uold, d_Unew, d_f, size, max_it, h);
-checkCudaErrors(cudaDeviceSynchronize());
+cudaDeviceSynchronize();
 
 k++;
 }
 
-checkCudaErrors(cudaMemcpy(h_Unew, d_Unew, total_size, cudaMemcpyDeviceToHost));
-
-int i;
-for (i = 0; i < size * size; i++) {
-if (i % size == 0)
-printf("\n");
-printf("%5.1f ", h_Unew[i]);}
-printf("\n");
+cudaMemcpy(h_Unew, d_Unew, total_size, cudaMemcpyDeviceToHost);
 
 
-checkCudaErrors(cudaFreeHost(h_Uold));
-checkCudaErrors(cudaFreeHost(h_Unew));
-checkCudaErrors(cudaFreeHost(h_f));
+cudaFreeHost(h_Uold);
+cudaFreeHost(h_Unew);
+cudaFreeHost(h_f);
 
-checkCudaErrors(cudaFree(d_Uold));
-checkCudaErrors(cudaFree(d_Unew));
-checkCudaErrors(cudaFree(d_f));
+cudaFree(d_Uold);
+cudaFree(d_Unew);
+cudaFree(d_f);
 
   return 0;
 }
--- assign3/poisson/gpu3/main.cu
+++ assign3/poisson/gpu3/main.cu
@@ -2,7 +2,6 @@
 #include "datatools.h"
 #include "init.h"
 #include "jacobian.h"
-#include <helper_cuda.h>
 #include <math.h>
 
 int main(int argc, char **argv) {
@@ -35,7 +34,7 @@ total_size = size * size * sizeof(double);
   init_u(size, h_Unew, guess);
   init_f(N, h_f);
 
-cudaSetDevice(6);
+cudaSetDevice(0);
 
 cudaMalloc((void**)&d0_Uold, total_size / 2);
 cudaMalloc((void**)&d0_Unew, total_size / 2);
@@ -45,7 +44,7 @@ cudaMemcpy(d0_Uold, h_Uold, total_size / 2, cudaMemcpyHostToDevice);
 cudaMemcpy(d0_Unew, h_Unew, total_size / 2, cudaMemcpyHostToDevice); 
 cudaMemcpy(d0_f, h_f, total_size / 2, cudaMemcpyHostToDevice); 
 
-cudaSetDevice(7);
+cudaSetDevice(1);
 
 cudaMalloc((void**)&d1_Uold, total_size / 2);
 cudaMalloc((void**)&d1_Unew, total_size / 2 );
@@ -55,14 +54,14 @@ cudaMemcpy(d1_Uold, h_Uold + size * size / 2, total_size / 2, cudaMemcpyHostToDe
 cudaMemcpy(d1_Unew, h_Unew + size * size / 2, total_size / 2, cudaMemcpyHostToDevice); 
 cudaMemcpy(d1_f, h_f + size * size / 2, total_size / 2, cudaMemcpyHostToDevice); 
 
-cudaDeviceEnablePeerAccess(6, 7);
+cudaDeviceEnablePeerAccess(0, 1);
 
 dim3 threadsPerBlock(16, 16);
-dim3 numBlocks(ceil(N/16.0/2), ceil(N/16.0));
+dim3 numBlocks(ceil(N/16.0/2), ceil(N/16./20));
 k = 0;
 while(k < max_it) {
 
-cudaSetDevice(6);
+cudaSetDevice(0);
 mat_swap(&d0_Uold, &d0_Unew);
 mat_swap(&d1_Uold, &d1_Unew);
 
@@ -70,37 +69,29 @@ mat_swap(&d1_Uold, &d1_Unew);
 cudaDeviceSynchronize();
 
 
-cudaSetDevice(7);
+cudaSetDevice(1);
   jacobian_1<<<numBlocks, threadsPerBlock>>>(d0_Uold, d1_Uold, d1_Unew, d1_f, size, max_it, h);
 cudaDeviceSynchronize();
 
 k++;
 }
 
-cudaSetDevice(6);
+cudaSetDevice(0);
 cudaMemcpy(h_Unew, d0_Unew, total_size / 2, cudaMemcpyDeviceToHost);
 
-cudaSetDevice(7);
+cudaSetDevice(1);
 cudaMemcpy(h_Unew + size * size / 2, d1_Unew, total_size / 2, cudaMemcpyDeviceToHost);
-/*
-int i;
-for (i = 0; i < size * size; i++) {
-if (i % size == 0)
-printf("\n");
-printf("%5.1f ", h_Unew[i]);}
-printf("\n");
-*/
 cudaFreeHost(h_Uold);
 cudaFreeHost(h_Unew);
 cudaFreeHost(h_f);
 
-cudaSetDevice(6);
+cudaSetDevice(0);
 cudaFree(d0_Uold);
 cudaFree(d0_Unew);
 cudaFree(d0_f);
 
 
-cudaSetDevice(7);
+cudaSetDevice(1);
 cudaFree(d1_Uold);
 cudaFree(d1_Unew);
 cudaFree(d1_f);
